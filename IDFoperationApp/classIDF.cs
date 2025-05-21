e94ecfcf33b57c738e6d2dc09383bd37fa3252a5
bezalelshteren <a0556722346@gmail.com>
2025-05-21 11:43:40 +0300

    added idf class

---
+++ IDFoperationApp/classIDF.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class IDF :IOrganizsion
+{
+
+
+
+
+
+
+
+}
+
+
+
+
+internal interface IStrikeOptions
+{
+    string AUniqueName { get; set; }
+
+    string AmmunitionCapacity { get; set; }
+
+    void FuelSupply();
+
+    List<string> typeOfTartet();
+}
+
