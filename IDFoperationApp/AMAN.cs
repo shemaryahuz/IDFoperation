3f709b8381bdc5529a63b11d9016b5d95d9d95af
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-21 15:59:33 +0300

    added aman class with create messag metthod and list of messages

---
+++ IDFoperationApp/AMAN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFoperationApp
+{
+    internal class AMAN
+    {
+        public List<IntelligencMessage> Messages = new List<IntelligencMessage>();
+        public void CreateMessage(Terrorist terrorist, string location, DateTime time)
+        {
+            IntelligencMessage message = new IntelligencMessage(terrorist, location, time);
+            this.Messages.Add(message);
+        }
+    }
+}
