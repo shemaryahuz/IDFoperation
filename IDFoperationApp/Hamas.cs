285d28bcf47d1008e020a020778052521c4765d2
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-21 11:56:12 +0300

    added hamas class

---
+++ IDFoperationApp/Hamas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFoperationApp
+{
+    internal class Hamas: IOrganization
+    {
+        public DateTime FormatDate { get; set; }
+        public string CurrentCommander { get; set; }
+
+    }
+}
