6e5884a1e4c187466e4b6ca7a2bf785f08e1f30f
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-05-30 16:27:59 +0300

    added prompts class to get prompts for sending to the gemini api

---
+++ IDFoperationApp/GeminiService/GeminiPrompts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFoperationApp
+{
+    internal static class GeminiPrompts
+    {
+        public static string GetTerroristPrompt()
+        {
+            return "Generate a JSON object for a Terrorist with the following properties: Name (string) , Rank (int), IsAlive(bool), Weapons (list of string (each string can be 'Rifle' or 'Gun' or 'Knife'))";
+        }
+    }
+}
